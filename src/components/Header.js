99bbafc0cf4cf4473e62c4f38dbb81f49fb69ff6
rbfridayy <rbfridayy@gmail.com>
2023-01-24 17:31:54 +0900

    gooey로고 테스트

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import "../assets/style/header.scss"
+
+function Header() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/Object.js
@@ -0,0 +1,13 @@
+import "./object.scss"
+
+function Object() {
+  return (
+    <div className="gooey">
+      <div>
+        <span></span>
+      </div>
+    </div>
+  );
+}
+
+export default Object;
